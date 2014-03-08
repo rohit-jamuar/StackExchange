5c9acb584b415e65b740624318b5e4d0a6536d3b
Rohit <rohitjamuar@hotmail.com>
2014-03-07 22:55:59 -0500

    Update newNodes.py

--- newNodes.py
+++ newNodes.py
@@ -1,81 +1,18 @@
 import csv
-f=open('NewNodes.txt','w')
+from sys import argv
+
 nodes={}
-with open('Android_2008.csv', 'rb') as csvfile:
-	x = csv.reader(csvfile, delimiter=',', )
-	temp=[]
-	for i in x:
-		if i[2]=='AskerId' : continue
-		if i[2] not in nodes:
-			nodes[i[2]]=i[3]
-			temp.append(i[2])
-		if i[7] not in nodes:
-			nodes[i[7]]=i[8]
-			temp.append(i[7])
-	f.write("New nodes in 2008 ="+str(len(temp))+"\n")
-
-with open('Android_2009.csv', 'rb') as csvfile:
-	x = csv.reader(csvfile, delimiter=',', )
-	temp=[]
-	for i in x:
-		if i[2]=='AskerId' : continue
-		if i[2] not in nodes:
-			nodes[i[2]]=i[3]
-			temp.append(i[2])
-		if i[7] not in nodes:
-			nodes[i[7]]=i[8]
-			temp.append(i[7])
-	f.write("New Nodes in 2009 ="+str(len(temp))+"\n")
-
-with open('Android_2010.csv', 'rb') as csvfile:
-	x = csv.reader(csvfile, delimiter=',', )
-	temp=[]
-	for i in x:
-		if i[2]=='AskerId' : continue
-		if i[2] not in nodes:
-			nodes[i[2]]=i[3]
-			temp.append(i[2])
-		if i[7] not in nodes:
-			nodes[i[7]]=i[8]
-			temp.append(i[7])
-	f.write("New Nodes in 2010 ="+str(len(temp))+"\n")
-
-with open('Android_2011.csv', 'rb') as csvfile:
-	x = csv.reader(csvfile, delimiter=',', )
-	temp=[]
-	for i in x:
-		if i[2]=='AskerId' : continue
-		if i[2] not in nodes:
-			nodes[i[2]]=i[3]
-			temp.append(i[2])
-		if i[7] not in nodes:
-			nodes[i[7]]=i[8]
-			temp.append(i[7])
-	f.write("New Nodes in 2011 ="+str(len(temp))+"\n")
-
-with open('Android_2012.csv', 'rb') as csvfile:
-	x = csv.reader(csvfile, delimiter=',', )
-	temp=[]
-	for i in x:
-		if i[2]=='AskerId' : continue
-		if i[2] not in nodes:
-			nodes[i[2]]=i[3]
-			temp.append(i[2])
-		if i[7] not in nodes:
-			nodes[i[7]]=i[8]
-			temp.append(i[7])
-	f.write("New Nodes in 2012 ="+str(len(temp))+"\n")
-
-with open('Android_2013.csv', 'rb') as csvfile:
-	x = csv.reader(csvfile, delimiter=',', )
-	temp=[]
-	for i in x:
-		if i[2]=='AskerId' : continue
-		if i[2] not in nodes:
-			nodes[i[2]]=i[3]
-			temp.append(i[2])
-		if i[7] not in nodes:
-			nodes[i[7]]=i[8]
-			temp.append(i[7])
-	f.write("New Nodes in 2013 ="+str(len(temp))+"\n")
-f.close()
+for j in argv[1:]:
+	f=open('NewNodes.txt','w')
+	with open(j, 'rb') as csvfile:
+		x = csv.reader(csvfile, delimiter=',', )
+		temp=[]
+		for i in x:
+			if i[2]=='AskerId' : continue
+			if i[2] not in nodes:
+				nodes[i[2]]=i[3]
+				temp.append(i[2])
+			if i[7] not in nodes:
+				nodes[i[7]]=i[8]
+				temp.append(i[7])
+		f.write("\nNew nodes from "+j+" = "+str(len(temp))+"\n\n")
