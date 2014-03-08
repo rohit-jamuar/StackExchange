d3d2876280f9a4bdda338c98284eee1edcf269ab
Rohit <rohitjamuar@hotmail.com>
2014-03-07 22:57:25 -0500

    Update newNodes.py

--- newNodes.py
+++ newNodes.py
@@ -3,16 +3,16 @@ from sys import argv
 
 nodes={}
 for j in argv[1:]:
-	f=open('NewNodes.txt','w')
-	with open(j, 'rb') as csvfile:
-		x = csv.reader(csvfile, delimiter=',', )
-		temp=[]
-		for i in x:
-			if i[2]=='AskerId' : continue
-			if i[2] not in nodes:
-				nodes[i[2]]=i[3]
-				temp.append(i[2])
-			if i[7] not in nodes:
-				nodes[i[7]]=i[8]
-				temp.append(i[7])
-		f.write("\nNew nodes from "+j+" = "+str(len(temp))+"\n\n")
+	with open('NewNodes.txt','w') as f:
+		with open(j, 'rb') as csvfile:
+			x = csv.reader(csvfile, delimiter=',', )
+			temp=[]
+			for i in x:
+				if i[2]=='AskerId' : continue
+				if i[2] not in nodes:
+					nodes[i[2]]=i[3]
+					temp.append(i[2])
+				if i[7] not in nodes:
+					nodes[i[7]]=i[8]
+					temp.append(i[7])
+			f.write("\nNew nodes from "+j+" = "+str(len(temp))+"\n\n")
